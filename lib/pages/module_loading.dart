b895277df3cfdd06a571a38a3f072c037a5a59c1
LukasDost <lukasdostal@dev.scalabs.de>
2021-12-08 20:26:57 +0100

    Markdown Converter

--- lib/pages/module_loading.dart
+++ lib/pages/module_loading.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:http/http.dart' as http;
 
 class Module extends StatelessWidget {
   const Module({Key? key}) : super(key: key);
 
+  final String urlG =
+      "https://raw.githubusercontent.com/wiki/wridgeu/wridgeu.github.io/Module%20Loading%20in%20UI5.md";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('dsa'),
       ),
-      body: TextButton(onPressed: getMarkdown, child: Text('dsa')),
+      body: FutureBuilder(
+        future: Future.delayed(Duration(seconds: 1),
+            () => rootBundle.loadString("assets/test.md")),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Markdown(
+              data: snapshot.data!,
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
-  }
-
-  Future<Markdown> getMarkdown() async {
-    var url = Uri.parse(
-        "https://raw.githubusercontent.com/wiki/wridgeu/wridgeu.github.io/Module%20Loading%20in%20UI5.md");
-    http.Response res = await http.get(url);
-    try {
-      if (res.statusCode == 200) {
-        String data = res.body;
-        return Markdown(data: data);
-      } else {
-        throw '';
-      }
-    } catch (e) {
-      throw '$e';
-    }
   }
 }
